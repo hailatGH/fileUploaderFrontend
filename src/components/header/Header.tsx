5acafb9f37c46650767f13181cab1162620c27f1
hailatGH <hailat.alx@gmail.com>
2023-04-01 12:17:21 +0300

    done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,12 @@
 import "./header.scss";
+import logo from "../../assets/Rectangle.png";
 
 function Header() {
   return (
     <div className="header">
-      <div className="logoWraper"></div>
+      <div className="logoWraper">
+        <img src={logo} />
+      </div>
     </div>
   );
 }
